618b4a3f8772734ab186540b507952271acb67ca
GabrielCG <gabrielbolso19@gmail.com>
2024-01-12 18:31:37 -0300

    modified the diagram to better suite tables, modified the sql query for the schema creation

--- EERDiagramScript.sql
+++ EERDiagramScript.sql
@@ -15,44 +15,6 @@ CREATE SCHEMA IF NOT EXISTS `LittleLemonDB` DEFAULT CHARACTER SET utf8 ;
 USE `LittleLemonDB` ;
 
 -- -----------------------------------------------------
--- Table `LittleLemonDB`.`OrderDeliveryStatus`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`OrderDeliveryStatus` (
-  `DeliveryStatusID` INT NOT NULL AUTO_INCREMENT,
-  `DeliveryDate` DATE NOT NULL,
-  `Status` TINYINT NOT NULL,
-  PRIMARY KEY (`DeliveryStatusID`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`MenuType`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`MenuType` (
-  `MenuTypeID` INT NOT NULL AUTO_INCREMENT,
-  `Type` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`MenuTypeID`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Menus`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menus` (
-  `MenuID` INT NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(45) NOT NULL,
-  `TypeID` INT NOT NULL,
-  PRIMARY KEY (`MenuID`),
-  INDEX `TypeID_idx` (`TypeID` ASC) VISIBLE,
-  CONSTRAINT `TypeID`
-    FOREIGN KEY (`TypeID`)
-    REFERENCES `LittleLemonDB`.`MenuType` (`MenuTypeID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `LittleLemonDB`.`Customers`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Customers` (
@@ -66,33 +28,107 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `LittleLemonDB`.`Orders`
+-- Table `LittleLemonDB`.`Bookings`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
-  `OrderID` INT NOT NULL AUTO_INCREMENT,
-  `OrderDate` DATE NOT NULL,
-  `Quantity` INT NOT NULL,
-  `TotalCost` DECIMAL(6,2) NOT NULL,
-  `MenuID` INT NOT NULL,
-  `StatusID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Bookings` (
+  `BookingID` INT NOT NULL AUTO_INCREMENT,
+  `Date` DATE NOT NULL,
+  `TableNo` INT NOT NULL,
   `CustomerID` INT NOT NULL,
-  PRIMARY KEY (`OrderID`),
-  INDEX `StatusID_idx` (`StatusID` ASC) VISIBLE,
-  INDEX `MenuID_idx` (`MenuID` ASC) VISIBLE,
+  PRIMARY KEY (`BookingID`),
   INDEX `CustomerID_idx` (`CustomerID` ASC) VISIBLE,
-  CONSTRAINT `StatusID`
-    FOREIGN KEY (`StatusID`)
-    REFERENCES `LittleLemonDB`.`OrderDeliveryStatus` (`DeliveryStatusID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `MenuID`
-    FOREIGN KEY (`MenuID`)
-    REFERENCES `LittleLemonDB`.`Menus` (`MenuID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `CustomerID`
     FOREIGN KEY (`CustomerID`)
     REFERENCES `LittleLemonDB`.`Customers` (`CustomerID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`OrderDeliveryStatus`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`OrderDeliveryStatus` (
+  `DeliveryStatusID` INT NOT NULL AUTO_INCREMENT,
+  `DeliveryDate` DATE NOT NULL,
+  `Status` TINYINT NOT NULL,
+  PRIMARY KEY (`DeliveryStatusID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Cuisine`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Cuisine` (
+  `CuisineID` INT NOT NULL AUTO_INCREMENT,
+  `Type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`CuisineID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Starter`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Starter` (
+  `StarterID` INT NOT NULL AUTO_INCREMENT,
+  `StarterName` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`StarterID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Course`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Course` (
+  `CourseID` INT NOT NULL AUTO_INCREMENT,
+  `CourseName` VARCHAR(100) NULL,
+  PRIMARY KEY (`CourseID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Drinks`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Drinks` (
+  `DrinkID` INT NOT NULL AUTO_INCREMENT,
+  `DrinkName` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`DrinkID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Menus`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menus` (
+  `MenuID` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(45) NOT NULL,
+  `CuisineID` INT NOT NULL,
+  `StarterID` INT NOT NULL,
+  `CourseID` INT NOT NULL,
+  `DrinkID` INT NOT NULL,
+  PRIMARY KEY (`MenuID`),
+  INDEX `TypeID_idx` (`CuisineID` ASC) VISIBLE,
+  INDEX `StarterID_idx` (`StarterID` ASC) VISIBLE,
+  INDEX `CourseID_idx` (`CourseID` ASC) VISIBLE,
+  INDEX `DrinkID_idx` (`DrinkID` ASC) VISIBLE,
+  CONSTRAINT `CuisineID`
+    FOREIGN KEY (`CuisineID`)
+    REFERENCES `LittleLemonDB`.`Cuisine` (`CuisineID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `StarterID`
+    FOREIGN KEY (`StarterID`)
+    REFERENCES `LittleLemonDB`.`Starter` (`StarterID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `CourseID`
+    FOREIGN KEY (`CourseID`)
+    REFERENCES `LittleLemonDB`.`Course` (`CourseID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `DrinkID`
+    FOREIGN KEY (`DrinkID`)
+    REFERENCES `LittleLemonDB`.`Drinks` (`DrinkID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -112,20 +148,35 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `LittleLemonDB`.`Bookings`
+-- Table `LittleLemonDB`.`Orders`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Bookings` (
-  `BookingID` INT NOT NULL AUTO_INCREMENT,
-  `Date` DATE NOT NULL,
-  `TableNo` INT NOT NULL,
-  `OrderID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
+  `OrderID` INT NOT NULL AUTO_INCREMENT,
+  `OrderDate` DATE NOT NULL,
+  `Quantity` INT NOT NULL,
+  `TotalCost` DECIMAL(6,2) NOT NULL,
+  `MenuID` INT NOT NULL,
+  `StatusID` INT NOT NULL,
+  `BookingID` INT NOT NULL,
   `StaffID` INT NULL,
-  PRIMARY KEY (`BookingID`),
-  INDEX `OrderID_idx` (`OrderID` ASC) VISIBLE,
+  PRIMARY KEY (`OrderID`),
+  INDEX `StatusID_idx` (`StatusID` ASC) VISIBLE,
+  INDEX `MenuID_idx` (`MenuID` ASC) VISIBLE,
+  INDEX `BookingID_idx` (`BookingID` ASC) VISIBLE,
   INDEX `StaffID_idx` (`StaffID` ASC) VISIBLE,
-  CONSTRAINT `OrderID`
-    FOREIGN KEY (`OrderID`)
-    REFERENCES `LittleLemonDB`.`Orders` (`OrderID`)
+  CONSTRAINT `StatusID`
+    FOREIGN KEY (`StatusID`)
+    REFERENCES `LittleLemonDB`.`OrderDeliveryStatus` (`DeliveryStatusID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `MenuID`
+    FOREIGN KEY (`MenuID`)
+    REFERENCES `LittleLemonDB`.`Menus` (`MenuID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `BookingID`
+    FOREIGN KEY (`BookingID`)
+    REFERENCES `LittleLemonDB`.`Bookings` (`BookingID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `StaffID`
---
+++ queries.sql
@@ -0,0 +1,28 @@
+-- USE littlelemondb;
+-- INSERT INTO MenuType(Type) VALUES("Course"), ("Starter"), ("Desert");
+
+-- INSERT INTO Staff (FirstName, LastName, `Role`, Salary)
+-- VALUES
+-- ('Sarah', 'Davis','Manager', 55000),
+-- ('Tom', 'Miller', 'Waiter', 30000),
+-- ('Linda', 'Jones', 'Chef', 40000),
+-- ('Robert', 'Walker', 'Cashier', 31000),
+-- ('Daniel', 'Robinson', 'Waiter', 32000),
+-- ('Susan', 'Allen', 'Hostess', 28000),
+-- ('Chris', 'Lewis', 'Manager', 60000),
+-- ('Jessica', 'Edwards', 'Chef', 38000),
+-- ('Brian', 'Collins', 'Waiter', 29000),
+-- ('Kim', 'Evans', 'Hostess', 27000);
+
+-- INSERT INTO Customers (FirstName, LastName, Phone, Email)
+-- VALUES
+-- ('John', 'Doe', 222-333-3333,'john.doe@example.com'),
+-- ('Jane', 'Powell', 111-444-3333, 'jane.doe@example.com'),
+-- ('Alice', 'Jenkins', 222-444-5555, 'alice@example.com'),
+-- ('Bob', 'Perry', 333-333-6666,'bob@example.com'),
+-- ('Charlie', 'Russell', 212-321-4545, 'charlie@example.com'),
+-- ('David', 'Sullivan', 344-4343-2222, 'david@example.com'),
+-- ('Emily', 'Ellis', 414-987-8765, 'emily@example.com'),
+-- ('Frank', 'Daniels', 132-131-9981, 'frank@example.com'),
+-- ('Grace', 'Richards', 454-345-1254, 'grace@example.com'),
+-- ('Hannah', 'Carpenter', 654-354-7676, 'hannah@example.com');
